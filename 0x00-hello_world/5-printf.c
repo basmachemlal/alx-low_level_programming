aa4660499ac8e9a371ebd1fb5bce6136890448c5
Basma <Basmachemlal56@gmail.com>
2023-03-16 16:26:15 -0700

    sd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * Description : main - Prints out a string to stdout.
- * Return : 0 if success.
+ * main - Entry point
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf ("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
 
