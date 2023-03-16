75e22f1f2d5d2310f8d7c71002d6e5b521f75f32
Basma <Basmachemlal56@gmail.com>
2023-03-16 15:24:05 -0700

    s:>

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-int main(void){
+#include <stdio.h>  
 /**
- * Description: main - prints out a string to stdout.
- * return: 0 if success
- */	
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+nt main(void){
 
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
-}
+};
